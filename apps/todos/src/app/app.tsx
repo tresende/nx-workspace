e2215890428948aaf509c2ba5e675e96a79c9d2f
Thiago Resende <thiago.resende@ifood.com.br>
2022-05-28 15:51:56 -0300

    feat(app): implements add todo

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,11 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+
+type Todo = {
+  title: string;
+};
 
 export function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { title: 'todo1' },
+    { title: 'todo2' },
+  ]);
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      { title: `New todo ${Math.floor(Math.random() * 1000)}` },
+    ]);
+  };
+
   return (
     <>
-      <NxWelcome title="todos" />
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li className="todo" key={todo.title}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <button id="add-todo" type="button" onClick={addTodo}>
+        Add Todo
+      </button>
       <div />
     </>
   );
